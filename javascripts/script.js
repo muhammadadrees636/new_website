30f2622367eb518f7ca84f088892d46781dacfc2
muhammadadrees3 <adreesservicesprovider@gmail.com>
2025-06-02 04:57:43 -0700

    uddate files

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,12 +1,12 @@
 
 const navBar = () => {
-    const manuBtn = document.querySelector(".navigation_show_btn")
-    const crossBtn = document.querySelector(".cross_btn")
-    var navigation = document.querySelector(".right_nav")
+    const manuBtn = document.querySelector(".navigation_show_btn");
+    const crossBtn = document.querySelector(".cross_btn");
+    var navigation = document.querySelector(".right_nav");
 
     manuBtn.addEventListener('click', () => {
-        navigation.classList.remove('hidden_navigation')
-        crossBtn.classList.remove('hidden')
+        navigation.classList.remove('hidden_navigation');
+        crossBtn.classList.remove('hidden');
     })
 
     crossBtn.addEventListener("click", () => {
@@ -15,3 +15,18 @@ const navBar = () => {
 }
 
 navBar()
+
+
+const fixesNavbar = () => {
+    var nav = document.querySelector('.nav');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 200) {
+            nav.classList.add('fixed_nav');
+        } else {
+            nav.classList.remove('fixed_nav');
+        }
+    });
+}
+
+fixesNavbar();
